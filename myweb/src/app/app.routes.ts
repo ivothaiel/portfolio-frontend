1064327b4255cf93de7f0b1b55d9bc705d01647d
Ivo <ivothaielvicencio@gmail.com>
2025-07-18 02:41:08 -0300

    definicion de las rutas

--- myweb/src/app/app.routes.ts
+++ myweb/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: "projects",
+        component: ProjectsComponent
+    },
+    {
+        path: "contact",
+        component: ContactComponent
+    },
+    {
+        path: "about",
+        component: AboutComponent
+    }
+];
